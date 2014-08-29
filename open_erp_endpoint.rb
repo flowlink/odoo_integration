0b6526dd44024bf4adf2a7954acb0cad0a778782
Washington Luiz <huoxito@gmail.com>
2014-08-28 23:02:45 -0300

    drop endpoints not ready yet

--- open_erp_endpoint.rb
+++ open_erp_endpoint.rb
@@ -8,16 +8,10 @@ class OpenErpEndpoint < EndpointBase::Sinatra::Base
 
   before do
     if @config
-      models = if request.path_info.include? "order"
-                 ["sale.order", "sale.shop", "stock.incoterms", "sale.order.line",
-                  "res.currency", "res.partner", "product.pricelist",
-                  "res.country", "res.country.state", "product.product",
-                  "account.tax"]
-               elsif request.path_info.include? "shipment"
-                 ["sale.order"]
-               else
-                 ["product.product"]
-               end
+      models = ["sale.order", "sale.shop", "stock.incoterms", "sale.order.line",
+                "res.currency", "res.partner", "product.pricelist",
+                "res.country", "res.country.state", "product.product",
+                "account.tax"]
 
       @client = OpenErp::Client.new(
         url: @config['openerp_api_url'],
@@ -26,21 +20,6 @@ class OpenErpEndpoint < EndpointBase::Sinatra::Base
         password: @config['openerp_api_password'],
         models: models
       )
-    end
-  end
-
-  post '/get_products' do
-    begin
-      products = @client.import_products
-      products.each { |p| add_object 'product', p }
-
-      if (count = products.count) > 0
-        result 200, "Received #{count} #{"product".pluralize count} from OpenERP"
-      else
-        result 200
-      end
-    rescue => e
-      result 500, e.message
     end
   end
 
@@ -59,28 +38,6 @@ class OpenErpEndpoint < EndpointBase::Sinatra::Base
       result 200, "The order #{@payload[:order][:id]} was updated in OpenERP"
     rescue => e
       result 500, e.message
-    end
-  end
-
-  post '/get_inventory' do
-    begin
-      stock = @client.update_stock(@payload)
-      add_object 'inventory', stock
-
-      result 200, "Inventory #{@payload[:inventory][:sku]} updated"
-    rescue => e
-      result 500, e.message
-    end
-  end
-
-  post '/get_shipments' do
-    begin
-      shipments = @client.confirm_shipment
-      shipments.each { |s| add_object 'shipment', s }
-
-      result 200, 'All pending shipments from OpenERP have been marked as shipped.'
-    rescue => e
-      result 500, "An OpenERP Endpoint error has occured: #{e.message}"
     end
   end
 end
