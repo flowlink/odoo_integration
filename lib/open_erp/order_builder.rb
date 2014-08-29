69ca3d30453d89d3ed973b623653ed5ecc882b73
Washington Luiz <huoxito@gmail.com>
2014-08-29 19:46:23 -0300

    No need to fetch SaleShop by ID

--- lib/open_erp/order_builder.rb
+++ lib/open_erp/order_builder.rb
@@ -27,7 +27,7 @@ module OpenErp
       order.partner_invoice_id = order.partner_id
       order.partner_shipping_id = set_partner_shipping_id(payload['order']['email'], order)
 
-      order.shop_id = SaleShop.find(id: config['openerp_shop'].to_i).first.id
+      order.shop_id = config[:openerp_shop]
 
       order.pricelist_id = set_pricelist(config['openerp_pricelist'])
       order.incoterm = StockIncoterms.find(:all, :domain => ['name', '=', config['openerp_shipping_name']]).first.try(:id)
