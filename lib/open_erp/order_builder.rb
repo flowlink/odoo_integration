5d27e3a9f8e2e24986287d857bf6bcf49332f990
Kim Goulbourne <kimberlygoulbourne@gmail.com>
2015-05-12 18:19:41 -0400

    test shop

--- lib/open_erp/order_builder.rb
+++ lib/open_erp/order_builder.rb
@@ -31,7 +31,7 @@ module OpenErp
       order.partner_invoice_id = 98 # order.partner_id
       order.partner_shipping_id = 98 #set_partner_shipping_id(payload['order']['email'], order)
 
-      order.shop_id = config[:openerp_shop]
+      order.shop_id = config['openerp_shop']
 
       order.pricelist_id = set_pricelist(config['openerp_pricelist'])
       order.incoterm = StockIncoterms.find(:all, :domain => ['name', '=', config['openerp_shipping_name']]).first.try(:id)
