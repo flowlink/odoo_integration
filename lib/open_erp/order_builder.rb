8e1a0ccbdcea6c1f8504bcfaaecdede46971b7af
Kim Goulbourne <kimberlygoulbourne@gmail.com>
2015-05-12 19:05:47 -0400

    test partner id

--- lib/open_erp/order_builder.rb
+++ lib/open_erp/order_builder.rb
@@ -15,7 +15,7 @@ module OpenErp
         raise OpenErpEndpointError, "All products in the order must exist on OpenERP"
       end
 
-      order = SaleOrder.new({
+      @order = SaleOrder.new({
         name: payload[:order][:id],
         date_order: Time.parse(payload['order']['placed_on']).strftime('%Y-%m-%d'),
         state: "done",
@@ -27,18 +27,18 @@ module OpenErp
       set_currency(order, payload['order']['currency'])
       set_customer(order, payload['order']['email'])
 
-      order.shipped = payload['order']['status'] == 'complete' ? true : false
-      order.partner_invoice_id = order.partner_id
-      order.partner_shipping_id = set_partner_shipping_id(payload['order']['email'], order)
+      @order.shipped = payload['order']['status'] == 'complete' ? true : false
+      @order.partner_invoice_id = @order.partner_id
+      @order.partner_shipping_id = set_partner_shipping_id(payload['order']['email'], order)
 
-      # order.shop_id = config['openerp_shop'] # is this needed? shop_id doesnt seem to exist anymore
+      # @order.shop_id = config['openerp_shop'] # is this needed? shop_id doesnt seem to exist anymore
 
-      order.pricelist_id = set_pricelist(config['openerp_pricelist'])
-      order.incoterm = StockIncoterms.find(:all, :domain => ['name', '=', config['openerp_shipping_name']]).first.try(:id)
+      @order.pricelist_id = set_pricelist(config['openerp_pricelist'])
+      @order.incoterm = StockIncoterms.find(:all, :domain => ['name', '=', config['openerp_shipping_name']]).first.try(:id)
       update_totals(order)
 
       # NOTE return here if order is not saved
-      order.save
+      @order.save
 
       # NOTE Check whether it's possible to sales order lines along with
       # the order in one single call
@@ -51,7 +51,7 @@ module OpenErp
       create_taxes_line(order) if order_payload['totals']['tax']
       create_discount_line(order)
 
-      order.reload
+      @order.reload
     end
 
     def update!
@@ -60,15 +60,15 @@ module OpenErp
       end
 
       order = find_order
-      order.partner_id = set_customer(order, payload['order']['email'])
-      order.partner_invoice_id = order.partner_id
-      order.partner_shipping_id = set_partner_shipping_id(payload['order']['email'], order)
-      order.shipped = payload['order']['status'] == 'complete' ? true : false
+      @order.partner_id = set_customer(order, payload['order']['email'])
+      @order.partner_invoice_id = @order.partner_id
+      @order.partner_shipping_id = set_partner_shipping_id(payload['order']['email'], order)
+      @order.shipped = payload['order']['status'] == 'complete' ? true : false
       update_totals(order)
 
-      order.save
+      @order.save
       update_line_items(order)
-      order.reload
+      @order.reload
     end
 
     private
@@ -81,11 +81,11 @@ module OpenErp
       end
 
       def update_totals(order)
-        order.amount_tax = payload['order']['totals']['tax'].to_f
+        @order.amount_tax = payload['order']['totals']['tax'].to_f
       end
 
       def find_order
-        return @sale_order if @sale_order ||= SaleOrder.find(name: "#{order_payload[:id]}").first
+        return @sale_order if @sale_order ||= Sale@Order.find(name: "#{order_payload[:id]}").first
         raise OpenErpEndpointError, "Order #{order[:number]} could not be found on OpenErp!"
       end
 
@@ -98,7 +98,7 @@ module OpenErp
       def update_line_items(order)
         payload[:order][:line_items].each do |li|
 
-          line = order.order_line.find { |line| line.product_id == li[:product_id] }
+          line = @order.order_line.find { |line| line.product_id == li[:product_id] }
 
           if line
             line.product_id = products.find { |p| p.default_code == line[:product_id] }.id
@@ -128,7 +128,7 @@ module OpenErp
       def create_line(line_payload, order)
         line = SaleOrderLine.new
         line.tax_id = line_payload[:tax_id].to_s.split(",") if line_payload[:tax_id]
-        line.order_id = order.id
+        line.order_id = @order.id
         line.name = line_payload[:name]
         line.product_id = products.find { |p| p.default_code == line_payload[:product_id] }.id
         line.product_uom_qty = line_payload[:quantity].to_f
@@ -138,7 +138,7 @@ module OpenErp
 
       def create_taxes_line(order)
         line = SaleOrderLine.new
-        line.order_id = order.id
+        line.order_id = @order.id
         line.name = "Taxes"
         line.product_uom_qty = 1.0
         line.price_unit = payload['order']['totals']['tax']
@@ -153,7 +153,7 @@ module OpenErp
         return if discount_adjustments.empty?
 
         line = SaleOrderLine.new
-        line.order_id = order.id
+        line.order_id = @order.id
         line.name = "Discounts"
         line.product_uom_qty = 1.0
         line.price_unit = discount_adjustments.map { |adj| adj[:value] }.reduce :+
@@ -163,7 +163,7 @@ module OpenErp
       def create_shipping_line(order)
         shipment_numbers = payload['order']['shipments'].map { |shipment| shipment['number'] }.join(', ')
         line = SaleOrderLine.new
-        line.order_id = order.id
+        line.order_id = @order.id
         line.name = "Shipping - #{shipment_numbers}"
         line.product_uom_qty = 1.0
         line.price_unit = payload['order']['totals']['shipping']
@@ -173,27 +173,27 @@ module OpenErp
       def set_picking_policy(order, policy)
         case policy
         when 'Deliver all products at once'
-          order.picking_policy = 'one'
+          @order.picking_policy = 'one'
         else
-          order.picking_policy = 'direct'
+          @order.picking_policy = 'direct'
         end
       end
 
       def set_order_policy(order, policy)
         case policy
         when 'On Delivery Order'
-          order.order_policy = 'picking'
+          @order.order_policy = 'picking'
         when 'On Demand'
-          order.order_policy = 'manual'
+          @order.order_policy = 'manual'
         else
-          order.order_policy = 'prepaid'
+          @order.order_policy = 'prepaid'
         end
       end
 
       def set_currency(order, currency)
         result = ResCurrency.find(name: currency)
         if result.length > 0
-          order.currency_id = result.first.id
+          @order.currency_id = result.first.id
         else
           raise OpenErpEndpointError, "Order currency #{currency} does not exist on OpenERP!"
         end
@@ -207,7 +207,7 @@ module OpenErp
                      OpenErp::CustomerManager.new(result.first, payload)
                    end
 
-        order.partner_id = 98 # customer.update!.id.to_i
+        @order.partner_id = 98 # customer.update!.id.to_i
       end
 
       def set_partner_shipping_id(email, order)
@@ -215,7 +215,7 @@ module OpenErp
         if result.length > 0
           98
         else
-          order.partner_id
+          @order.partner_id
         end
       end
 
