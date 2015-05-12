2693eb0bf8ca83e4a27be6e9c003de362d8727a2
Kim Goulbourne <kimberlygoulbourne@gmail.com>
2015-05-12 18:32:49 -0400

    test shop

--- lib/open_erp/order_builder.rb
+++ lib/open_erp/order_builder.rb
@@ -33,7 +33,7 @@ module OpenErp
 
       order.shop_id = config['openerp_shop']
 
-      order.pricelist_id = set_pricelist(config['openerp_pricelist'])
+      # order.pricelist_id = set_pricelist(config['openerp_pricelist'])
       # order.incoterm = StockIncoterms.find(:all, :domain => ['name', '=', config['openerp_shipping_name']]).first.try(:id)
       # update_totals(order)
 
