00f6ba24b5fb4ea091b5111598b0e98301600d9d
Kim Goulbourne <kimberlygoulbourne@gmail.com>
2015-05-12 18:07:19 -0400

    test ntn

--- lib/open_erp/order_builder.rb
+++ lib/open_erp/order_builder.rb
@@ -27,15 +27,15 @@ module OpenErp
       set_currency(order, payload['order']['currency'])
       set_customer(order, payload['order']['email'])
 
-      # order.shipped = payload['order']['status'] == 'complete' ? true : false
-      # order.partner_invoice_id = order.partner_id
+      order.shipped = payload['order']['status'] == 'complete' ? true : false
+      order.partner_invoice_id = order.partner_id
       # order.partner_shipping_id = set_partner_shipping_id(payload['order']['email'], order)
 
       # order.shop_id = config[:openerp_shop]
 
       # order.pricelist_id = set_pricelist(config['openerp_pricelist'])
       # order.incoterm = StockIncoterms.find(:all, :domain => ['name', '=', config['openerp_shipping_name']]).first.try(:id)
-      # update_totals(order)
+      update_totals(order)
 
       # NOTE return here if order is not saved
       order.save
