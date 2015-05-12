1b58813a24162193dbb3e145fb774ce31141c21b
Kim Goulbourne <kimberlygoulbourne@gmail.com>
2015-05-12 18:36:13 -0400

    test shop

--- lib/open_erp/order_builder.rb
+++ lib/open_erp/order_builder.rb
@@ -33,25 +33,25 @@ module OpenErp
 
       # order.shop_id = config['openerp_shop']
 
-      # order.pricelist_id = set_pricelist(config['openerp_pricelist'])
-      # order.incoterm = StockIncoterms.find(:all, :domain => ['name', '=', config['openerp_shipping_name']]).first.try(:id)
-      # update_totals(order)
+      order.pricelist_id = set_pricelist(config['openerp_pricelist'])
+      order.incoterm = StockIncoterms.find(:all, :domain => ['name', '=', config['openerp_shipping_name']]).first.try(:id)
+      update_totals(order)
 
-      # # NOTE return here if order is not saved
-      # order.save
+      # NOTE return here if order is not saved
+      order.save
 
-      # # NOTE Check whether it's possible to sales order lines along with
-      # # the order in one single call
+      # NOTE Check whether it's possible to sales order lines along with
+      # the order in one single call
 
-      # set_line_items(order)
+      set_line_items(order)
 
-      # # NOTE Wombat default order object has no shipments
-      # # create_shipping_line(order)
+      # NOTE Wombat default order object has no shipments
+      # create_shipping_line(order)
 
-      # create_taxes_line(order) if order_payload['totals']['tax']
-      # create_discount_line(order)
+      create_taxes_line(order) if order_payload['totals']['tax']
+      create_discount_line(order)
 
-      # order.reload
+      order.reload
     end
 
     def update!
