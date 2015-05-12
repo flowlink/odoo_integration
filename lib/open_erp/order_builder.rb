70843dc0273ba3586b1e1cc9f34deb01ffd1261d
Kim Goulbourne <kimberlygoulbourne@gmail.com>
2015-05-12 18:11:10 -0400

    test id

--- lib/open_erp/order_builder.rb
+++ lib/open_erp/order_builder.rb
@@ -29,12 +29,12 @@ module OpenErp
 
       order.shipped = payload['order']['status'] == 'complete' ? true : false
       order.partner_invoice_id = order.partner_id
-      # order.partner_shipping_id = set_partner_shipping_id(payload['order']['email'], order)
+      order.partner_shipping_id = set_partner_shipping_id(payload['order']['email'], order)
 
-      # order.shop_id = config[:openerp_shop]
+      order.shop_id = config[:openerp_shop]
 
-      # order.pricelist_id = set_pricelist(config['openerp_pricelist'])
-      # order.incoterm = StockIncoterms.find(:all, :domain => ['name', '=', config['openerp_shipping_name']]).first.try(:id)
+      order.pricelist_id = set_pricelist(config['openerp_pricelist'])
+      order.incoterm = StockIncoterms.find(:all, :domain => ['name', '=', config['openerp_shipping_name']]).first.try(:id)
       update_totals(order)
 
       # NOTE return here if order is not saved
@@ -207,7 +207,7 @@ module OpenErp
                      OpenErp::CustomerManager.new(result.first, payload)
                    end
 
-        order.partner_id = customer.update!.id
+        order.partner_id = 98 #customer.update!.id
       end
 
       def set_partner_shipping_id(email, order)
