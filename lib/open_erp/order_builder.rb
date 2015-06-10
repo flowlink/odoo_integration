9e49a1f24a88ea4847d952aaccf1c06a8c5da507
Kim Goulbourne <kimberlygoulbourne@gmail.com>
2015-06-10 14:59:34 -0400

    try workflow directly on order object

--- lib/open_erp/order_builder.rb
+++ lib/open_erp/order_builder.rb
@@ -52,7 +52,7 @@ module OpenErp
       create_discount_line(order)
 
       # execute workflow
-      SaleOrder.rpc_exec_workflow('order_confirm', payload[:order][:id])
+      order.rpc_exec_workflow('order_confirm', payload[:order][:id])
 
       order.reload
     end
